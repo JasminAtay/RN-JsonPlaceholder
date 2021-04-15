9320cdeace66b9bde8d50d0145c97eb63d6d85d1
Jasmin Atay <yasminatayy@icloud.com>
2021-04-15 18:08:29 +0200

    Navigations are added

--- App.js
+++ App.js
@@ -1,21 +1,41 @@
-import { StatusBar } from 'expo-status-bar';
+import { NavigationContainer } from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack'; 
+import {createDrawerNavigator} from '@react-navigation/drawer'; 
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {IntroScreen, ListScreen, DetailsScreen, ThemesScreen, CreditsScreen} from './src/screens';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const Stack = createStackNavigator();
+const SideMenu = createDrawerNavigator();
+
+const MainFlow = () =>{
+    return(
+        <Stack.Navigator initialRouteName="Intro">
+                <Stack.Screen 
+                name="Intro" 
+                component={IntroScreen} 
+                options= {{headerShown: false}}
+                />
+                <Stack.Screen name="List" component={ListScreen}/>
+                <Stack.Screen name="Details" component={DetailsScreen}/>
+            </Stack.Navigator>
+    );
+};
+
+
+const App = () => {
+    return(
+        <NavigationContainer>
+            <SideMenu.Navigator initialRouteName="Main" >
+                <SideMenu.Screen name="Themes" component={ThemesScreen}/>
+                <SideMenu.Screen name="Credits" component={CreditsScreen}/>
+                <SideMenu.Screen name="Main" component={MainFlow}/>
+
+            </SideMenu.Navigator>
+        </NavigationContainer>
+    );
+};
+
+
+
+export default App;
