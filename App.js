360e18a57b3e6802c3016fa3035874304561d45b
Jasmin Atay <yasminatayy@icloud.com>
2021-04-15 20:05:02 +0200

    Refactor navigator

--- App.js
+++ App.js
@@ -1,41 +1,8 @@
-import { NavigationContainer } from '@react-navigation/native';
-import {createStackNavigator} from '@react-navigation/stack'; 
-import {createDrawerNavigator} from '@react-navigation/drawer'; 
 import React from 'react';
-import {IntroScreen, ListScreen, DetailsScreen, ThemesScreen, CreditsScreen} from './src/screens';
-
-
-const Stack = createStackNavigator();
-const SideMenu = createDrawerNavigator();
-
-const MainFlow = () =>{
-    return(
-        <Stack.Navigator initialRouteName="Intro">
-                <Stack.Screen 
-                name="Intro" 
-                component={IntroScreen} 
-                options= {{headerShown: false}}
-                />
-                <Stack.Screen name="List" component={ListScreen}/>
-                <Stack.Screen name="Details" component={DetailsScreen}/>
-            </Stack.Navigator>
-    );
-};
+import {Navigator} from './src/navigation/Navigator';
 
 
 const App = () => {
-    return(
-        <NavigationContainer>
-            <SideMenu.Navigator initialRouteName="Main" >
-                <SideMenu.Screen name="Themes" component={ThemesScreen}/>
-                <SideMenu.Screen name="Credits" component={CreditsScreen}/>
-                <SideMenu.Screen name="Main" component={MainFlow}/>
-
-            </SideMenu.Navigator>
-        </NavigationContainer>
-    );
+    return  <Navigator/>;
 };
-
-
-
 export default App;
---
+++ src/navigation/SideMenu.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import {createDrawerNavigator} from '@react-navigation/drawer'; 
+import {ThemesScreen, CreditsScreen} from '../screens';
+import {MainFlow} from './MainFlow'
+
+
+const Drawer = createDrawerNavigator();
+
+const SideMenu = () => {
+    return(
+            <Drawer.Navigator initialRouteName="Main" >
+                <Drawer.Screen name="Themes" component={ThemesScreen}/>
+                <Drawer.Screen name="Credits" component={CreditsScreen}/>
+                <Drawer.Screen name="Main" component={MainFlow}/>
+            </Drawer.Navigator>
+       
+    );
+};
+
+
+
